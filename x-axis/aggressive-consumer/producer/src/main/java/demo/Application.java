cd671a663815d0e01fcdb296a39eff295b6e038f
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-13 00:20:51 +0100

    adding app

--- x-axis/aggressive-consumer/producer/src/main/java/demo/Application.java
+++ x-axis/aggressive-consumer/producer/src/main/java/demo/Application.java
@@ -1,7 +1,10 @@
 package demo;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +18,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner producer(RabbitTemplate rabbitTemplate) {
+        return args -> {
+            System.out.println(rabbitTemplate.convertSendAndReceive("pings", "Josh"));
+        };
     }
 }
