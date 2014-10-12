ae057f7ecddae1f857a3c08d94654aa2a958a30e
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-13 00:55:03 +0100

    request/reply

--- x-axis/aggressive-consumer/consumer/src/main/java/demo/Application.java
+++ x-axis/aggressive-consumer/consumer/src/main/java/demo/Application.java
@@ -1,6 +1,10 @@
 package demo;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -22,14 +26,25 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        LoggerFactory.getLogger(Application.class).info("Started consumer.");
+
+    }
+
+    @Bean
+    InitializingBean initializingBean(AmqpAdmin amqpAdmin) {
+        return () -> amqpAdmin.declareQueue(new Queue("pings"));
     }
 
     @Bean
     IntegrationFlow incomingRequests(ConnectionFactory connectionFactory) {
-
         return IntegrationFlows.from(Amqp.inboundGateway(connectionFactory, "pings"))
                 .transform("hello "::concat)
                 .transform(String.class, String::toUpperCase)
+                .<String, String>transform(m -> {
+                    LoggerFactory.getLogger(Application.class).info("replying  " + m);
+                    return m;
+                })
                 .get();
     }
 
